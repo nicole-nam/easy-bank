1534de4f4c54513d229e124e9ddef8c3a909e07d
nicole <eunjungnam@gmail.com>
2020-11-26 15:53:30 -0500

    fix hamburger rotation on click

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ btnHamburger.addEventListener('click', function(){
 if(btnHamburger.classList.contains('open')){
     btnHamburger.classList.remove('open');
 }else {
-    btnHamburger.classList.add('open');
+    btnHamburger.classList.add('open')
 }
 
 });
