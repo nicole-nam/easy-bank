4f850cd9e689cf58ef93129ba192d3918066ef07
nicole <eunjungnam@gmail.com>
2020-11-27 11:32:50 -0500

    add function to toggle header menu style on click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const headerMenu = document.querySelector('.header-menu');
 
 btnHamburger.addEventListener('click', function(){
 
-if(header.classList.contains('open')){
-    header.classList.remove('open');
-}else {
-    header.classList.add('open')
-}
+    if(header.classList.contains('open') ){
+        header.classList.remove('open');
+        headerMenu.style.display = "none";
+     
+    }else  {
+        header.classList.add('open');
+        headerMenu.style.display = "block";
+    }
+
 
 });
     
