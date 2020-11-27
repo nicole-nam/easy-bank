ac2c8a137f6d32563a182baaf711819459901db3
nicole <eunjungnam@gmail.com>
2020-11-27 12:06:21 -0500

    add sticky header function and style

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const header = document.querySelector('.header');
 const headerMenu = document.querySelector('.header-menu');
 const medaiQuery = window.matchMedia('(max-width: 768px)');
 
+
 btnHamburger.addEventListener('click', function(){
 
     if(header.classList.contains('open')){
@@ -11,15 +12,29 @@ btnHamburger.addEventListener('click', function(){
      
     }else  {
         header.classList.add('open');
-        headerMenu.style.display = "block";
+        headerMenu.style.display = 'block';
     }
 
 });
     
-medaiQuery.addEventListener("change", (e) => {
+medaiQuery.addEventListener('change', (e) => {
     if(e.matches && header.classList.contains('open')){
-        headerMenu.style.display ="block";
+        headerMenu.style.display ='block';
     }else {
-        headerMenu.style.display = "none";
+        headerMenu.style.display = 'none';
     }
 })
+
+//fixed header
+
+window.onscroll = function() {stickyHeader()};
+
+const sticky = header.offsetTop;
+
+function stickyHeader(){
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove('sticky');
+    }
+}
