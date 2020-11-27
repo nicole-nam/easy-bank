c7e3d5d244f6b6a8d66251abd0aedc31eeff6ca0
nicole <eunjungnam@gmail.com>
2020-11-27 11:55:18 -0500

    add eventListener to media query

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const headerMenu = document.querySelector('.header-menu');
+const medaiQuery = window.matchMedia('(max-width: 768px)');
 
 btnHamburger.addEventListener('click', function(){
 
-    if(header.classList.contains('open') ){
+    if(header.classList.contains('open')){
         header.classList.remove('open');
         headerMenu.style.display = "none";
      
@@ -13,6 +14,12 @@ btnHamburger.addEventListener('click', function(){
         headerMenu.style.display = "block";
     }
 
-
 });
     
+medaiQuery.addEventListener("change", (e) => {
+    if(e.matches && header.classList.contains('open')){
+        headerMenu.style.display ="block";
+    }else {
+        headerMenu.style.display = "none";
+    }
+})
